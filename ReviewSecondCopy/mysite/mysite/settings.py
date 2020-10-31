b9a42c2e9901ad478d40b4f9cd5099a1fc523752
BlackCode10 <73743237+BlackCode10@users.noreply.github.com>
2020-10-31 07:43:07 -0600

    Update settings.py

--- ReviewSecondCopy/mysite/mysite/settings.py
+++ ReviewSecondCopy/mysite/mysite/settings.py
@@ -15,7 +15,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -128,4 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
